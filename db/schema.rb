cb7a1450cef7f83fea9b31d9878c4f661053ba1d
Walid Kabboussa <walidkabou@gmail.com>
2023-07-10 13:01:59 +0100

    Create Migration file for User

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_10_101240) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_10_111142) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "fullname"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -23,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_10_101240) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
