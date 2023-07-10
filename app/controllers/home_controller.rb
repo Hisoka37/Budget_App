3736c85f7b0cd353ade79b4c237ac97d166740fb
Walid Kabboussa <walidkabou@gmail.com>
2023-07-10 14:53:08 +0100

    Generate and Setup Controllers

--- app/controllers/home_controller.rb
+++
@@ -1,5 +0,0 @@
-class HomeController < ActionController::Base
-    def homepage
-    
-    end
-end
---
+++ app/models/category.rb
@@ -0,0 +1,8 @@
+class Category < ApplicationRecord
+    belongs_to :user
+    has_many :payments, dependent: :destroy
+  
+    validates :name, presence: true, length: { maximum: 60 }
+    validates :icon, presence: true
+    
+end
