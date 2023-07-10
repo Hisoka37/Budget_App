4af7a1e833e954e8ed788a207d52d1b215bcef8d
Walid Kabboussa <walidkabou@gmail.com>
2023-07-10 18:54:59 +0100

    Install Rubocop and format code

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,7 @@
 class Category < ApplicationRecord
-    belongs_to :user
-    has_many :payments, dependent: :destroy
-  
-    validates :name, presence: true, length: { maximum: 60 }
-    validates :icon, presence: true
-    
+  belongs_to :user
+  has_many :payments, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 60 }
+  validates :icon, presence: true
 end
